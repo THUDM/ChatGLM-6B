a2f8bec32beab69608793170de51640d84f73d05
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2023-03-13 23:38:20 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -2,8 +2,7 @@ from transformers import AutoModel, AutoTokenizer
 import gradio as gr
 
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = model.half().cuda()
+model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
 model = model.eval()
 
 MAX_TURNS = 20
