fc4ac835014200a3ef4d6f4795572f41b325265e
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2023-03-14 20:39:55 +0800

    Add prompt after clear

--- cli_demo.py
+++ cli_demo.py
@@ -17,7 +17,8 @@ while True:
     if query == "clear":
         history = []
         command = 'cls' if os_name == 'Windows' else 'clear'
-        os.system(command) 
+        os.system(command)
+        print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
         continue
     response, history = model.chat(tokenizer, query, history=history)
     print(f"ChatGLM-6B：{response}")
