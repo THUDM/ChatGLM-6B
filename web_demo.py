e565185c2906dfc61ecfff2add8a9ab9a87c4158
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2023-03-16 00:44:07 +0800

    Add inbrower for web demo

--- web_demo.py
+++ web_demo.py
@@ -37,4 +37,4 @@ with gr.Blocks() as demo:
         with gr.Column(scale=1):
             button = gr.Button("Generate")
     button.click(predict, [txt, state], [state] + text_boxes)
-demo.queue().launch(share=True)
+demo.queue().launch(share=True, inbrowser=True)
