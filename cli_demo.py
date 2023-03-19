17ecc572662ea6aa1fa2fd3fec01d6add83ef337
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2023-03-19 14:56:34 +0800

    Add newline in cli output

--- cli_demo.py
+++ cli_demo.py
@@ -13,8 +13,8 @@ clear_command = 'cls' if os_name == 'Windows' else 'clear'
 def build_prompt(history):
     prompt = "欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序"
     for query, response in history:
-        prompt += f"\n用户：{query}"
-        prompt += f"\nChatGLM-6B：{response}"
+        prompt += f"\n\n用户：{query}"
+        prompt += f"\n\nChatGLM-6B：{response}"
     return prompt
 
 
