d2645d881653d5605a8e20a1d4725188493d8c3b
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2023-03-31 11:28:13 +0800

    Update batch size

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -8,14 +8,14 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path THUDM/chatglm-6b \
+    --model_name_or_path /mnt/vepfs/workspace/zxdu/chatglm_6b \
     --output_dir output/adgen-chatglm-6b-pt-$PRE_SEQ_LEN-$LR-dev \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 64 \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 16 \
     --predict_with_generate \
     --max_steps 3000 \
     --logging_steps 10 \
