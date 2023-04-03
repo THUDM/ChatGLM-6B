6148d6d6ac41a416846df1ebfa9b1341a1f69859
tuteng0915 <leotuteng@126.com>
2023-04-03 23:11:31 +0800

    add web_demo3

---
+++ web_demo3.py
@@ -0,0 +1,69 @@
+from transformers import AutoModel, AutoTokenizer
+import gradio as gr
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Tuple, Type
+import mdtex2html
+
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).quantize(8).half().cuda()
+model = model.eval()
+
+# MAX_TURNS = 20
+# MAX_BOXES = MAX_TURNS * 2
+
+"""Override Chatbot.postprocess"""
+def postprocess(self, y):
+        if y is None:
+            return []
+        for i, (message, response) in enumerate(y):
+            y[i] = (
+                None if message is None else mdtex2html.convert((message)),
+                None if response is None else mdtex2html.convert(response),
+            )
+        return y
+gr.Chatbot.postprocess = postprocess
+
+
+def predict(input, chatbot, max_length, top_p, temperature, history):
+    chatbot.append((input, ""))
+    for response, history in model.stream_chat(tokenizer, input, history, max_length=max_length, top_p=top_p,
+                                               temperature=temperature):
+        chatbot[-1] = (input, response)       
+        yield chatbot, history
+
+def reset_user_input():
+    return gr.update(value='')
+
+
+def reset_state():
+    return [], []
+
+with gr.Blocks() as demo:
+    gr.HTML("""<h1 align="center">ChatGLM</h1>""")
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            chatbot = gr.Chatbot()
+            with gr.Row():
+                with gr.Column(scale=12):
+                    user_input = gr.Textbox(show_label=False, placeholder="Input...").style(
+                        container=False)
+                with gr.Column(min_width=32, scale=1):
+                    submitBtn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+    
+    history = gr.State([])
+
+    user_input.submit(predict, [user_input, chatbot, max_length, top_p, temperature, history], [chatbot, history], show_progress=True)
+    user_input.submit(reset_user_input, [], [user_input])
+
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history], [chatbot, history], show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+
+
+demo.queue().launch(share=False, inbrowser=True)
