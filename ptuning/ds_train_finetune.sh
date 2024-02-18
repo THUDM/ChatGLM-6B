a0dc8f774a7fa11d3828a87775bbc694d62c1477
SimonLee <lixiaomin_235@163.com>
2024-02-18 10:58:08 +0800

    fix error when only 1 gpu

--- ptuning/ds_train_finetune.sh
+++ ptuning/ds_train_finetune.sh
@@ -3,7 +3,7 @@ LR=1e-4
 
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 
-deepspeed --num_gpus=4 --master_port $MASTER_PORT main.py \
+deepspeed --num_gpus=1 --master_port $MASTER_PORT main.py \
     --deepspeed deepspeed.json \
     --do_train \
     --train_file AdvertiseGen/train.json \
